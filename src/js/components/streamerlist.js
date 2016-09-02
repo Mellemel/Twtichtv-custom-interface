f3ec2520f711d00198ae455c56a029a8ecaaacd2
Melvin Canela <M.Canela92@outlook.com>
2016-09-02 14:20:07 -0400

    sort streamer function by online status implemented

--- src/js/components/streamerlist.js
+++ src/js/components/streamerlist.js
@@ -4,27 +4,34 @@ import Streamer from './streamer'
 class StreamerList extends React.Component {
   constructor() {
     super()
-    this.state = {
-      streamers: []
-    }
-    let streamers = ['riotgames', 'starladder1', 'beyondthesummit', 'esl_csgo', 'tsm_theoddone', 'tsm_dyrus', 'imaqtpie', 'garenatw', 'Nightblue3', 'nl_kripp', 'freecodecamp']
 
-    this.state.streamers = streamers.map((name) => {
+    this.state = {
+      streamers: ['riotgames', 'starladder1', 'beyondthesummit', 'esl_csgo', 'tsm_theoddone', 'tsm_dyrus', 'imaqtpie', 'garenatw', 'Nightblue3', 'nl_kripp', 'freecodecamp']
+    }
+
+    this.state.streamers = this.state.streamers.map((name) => {
       return { name: name, online: false }
     })
+
+    this.statusChange = this.statusChange.bind(this)
   }
-  statusChange(name){
-    this.state.streamers.
+
+  statusChange(name) {
+    this.state.streamers.forEach(function (streamer) {
+      if (streamer.name == name) {
+        streamer.online = true
+      }
+    })
     this.sortStreamersByStatus()
   }
+
   sortStreamersByStatus() {
-    console.log(this.state.streamers)
-    this.state.streamers.sort((a, b) => {
-      return (a === b) ? 0 : a ? -1 : 1
+    this.state.streamers = this.state.streamers.sort((a, b) => {
+      return (a.online === b.online) ? 0 : a.online ? -1 : 1
     })
-    console.log(this.state.streamers)
     this.setState({ streamers: this.state.streamers })
   }
+
   render() {
     var streamers = this.state.streamers.map((streamer) => {
       return <Streamer key={streamer.name} name={streamer.name} onStatusChange={this.statusChange}/>
